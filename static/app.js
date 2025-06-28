db8fc54390b5a57df537cd4c106ca67a836725c9
sriharshamareedu <119287958+sriharshamareedu@users.noreply.github.com>
2025-06-28 18:28:34 +0530

    Update app.js

--- static/app.js
+++ static/app.js
@@ -45,7 +45,8 @@ function onPageLoad() {
       $.get(url,function(data, status) {
       console.log("got response for get_location_names request");
       if(data) {
-          var locations = data.locations;
+          //var locations = data.locations;
+          var locations = data; // FIXED HERE
           var uiLocations = document.getElementById("uiLocations");
           $('#uiLocations').empty();
           for(var i in locations) {
